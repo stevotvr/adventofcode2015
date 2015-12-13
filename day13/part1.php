9d6b35b9ddb7260def86d024dd924f1b276f16c4
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-12-13 15:13:53 -0800

    Add day 13

---
+++ day13/part1.php
@@ -0,0 +1,40 @@
+<?php
+
+$lines = file('input.txt');
+$data = array();
+
+foreach($lines as $line) {
+    if(preg_match('/(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)/i', $line, $matches)) {
+        $value = $matches[2] === 'gain' ? (int)$matches[3] : -(int)$matches[3];
+        $data[$matches[1]][$matches[4]] = $value;
+    }
+}
+
+function getPermutations(array $items, &$result, array $perms = array()) {
+    if(empty($items)) {
+        $result[] = $perms;
+    } else {
+        for($i = 0; $i < count($items); $i++) {
+            $newItems = $items;
+            $newPerms = $perms;
+            list($item) = array_splice($newItems, $i, 1);
+            $newPerms[] = $item;
+            getPermutations($newItems, $result, $newPerms);
+        }
+    }
+}
+
+getPermutations(array_keys($data), $permutations);
+$totalHappiness = -PHP_INT_MAX;
+foreach($permutations as $permutation) {
+    $happiness = 0;
+    for($i = 0, $count = count($permutation); $i < $count; $i++) {
+        $subject = $data[$permutation[$i]];
+        $left = $permutation[($i === 0 ? $count : $i) - 1];
+        $right = $permutation[$i === $count - 1 ? 0 : $i + 1];
+        $happiness += $subject[$left] + $subject[$right];
+    }
+    $totalHappiness = max(array($totalHappiness, $happiness));
+}
+
+echo 'Answer: ' . $totalHappiness;
---
+++ day13/part2.php
@@ -0,0 +1,46 @@
+<?php
+
+$lines = file('input.txt');
+$data = array();
+
+foreach($lines as $line) {
+    if(preg_match('/(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)/i', $line, $matches)) {
+        $value = $matches[2] === 'gain' ? (int)$matches[3] : -(int)$matches[3];
+        $data[$matches[1]][$matches[4]] = $value;
+    }
+}
+
+$me = 'Steve';
+foreach(array_keys($data) as $key) {
+    $data[$me][$key] = $data[$key][$me] = 0;
+}
+
+function getPermutations(array $items, &$result, array $perms = array()) {
+    if(empty($items)) {
+        $result[] = implode('|', $perms);
+    } else {
+        for($i = 0; $i < count($items); $i++) {
+            $newItems = $items;
+            $newPerms = $perms;
+            list($item) = array_splice($newItems, $i, 1);
+            $newPerms[] = $item;
+            getPermutations($newItems, $result, $newPerms);
+        }
+    }
+}
+
+getPermutations(array_keys($data), $permutations);
+$totalHappiness = -PHP_INT_MAX;
+foreach($permutations as $permutation) {
+    $perm = explode('|', $permutation);
+    $happiness = 0;
+    for($i = 0, $count = count($perm); $i < $count; $i++) {
+        $subject = $data[$perm[$i]];
+        $left = $perm[($i === 0 ? $count : $i) - 1];
+        $right = $perm[$i === $count - 1 ? 0 : $i + 1];
+        $happiness += $subject[$left] + $subject[$right];
+    }
+    $totalHappiness = max(array($totalHappiness, $happiness));
+}
+
+echo 'Answer: ' . $totalHappiness;
