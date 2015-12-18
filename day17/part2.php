f147f0d114bb767ea5396341c6d3152308bad61b
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-12-17 20:38:00 -0800

    Fix day 17 part 2

--- day17/part2.php
+++ day17/part2.php
@@ -3,19 +3,23 @@
 $data = array(11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1, 19, 3);
 rsort($data);
 
-function countCombinations(array $data, $total, &$count) {
+function countCombinations(array $data, $total, array &$counts, $num = 1) {
     while(count($data) > 0) {
         $remaining = $total - array_shift($data);
         if($remaining > 0) {
-            countCombinations($data, $remaining, $count);
+            countCombinations($data, $remaining, $counts, $num + 1);
         } elseif($remaining === 0) {
-            $count++;
-        } else {
-            $count = 0;
+            $counts[$num]++;
         }
     }
 }
 
-countCombinations($data, 150, $count);
+$counts = array_fill(1, count($data), 0);
+countCombinations($data, 150, $counts);
 
-echo 'Answer: ' . $count;
+foreach($counts as $count) {
+    if($count > 0) {
+        echo 'Answer: ' . $count;
+        break;
+    }
+}
