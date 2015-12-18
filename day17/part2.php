0db3f09b8ef8b9a05a508fe113625ca5e66dfd4e
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-12-17 16:40:23 -0800

    Add day 17

---
+++ day17/part2.php
@@ -0,0 +1,21 @@
+<?php
+
+$data = array(11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1, 19, 3);
+rsort($data);
+
+function countCombinations(array $data, $total, &$count) {
+    while(count($data) > 0) {
+        $remaining = $total - array_shift($data);
+        if($remaining > 0) {
+            countCombinations($data, $remaining, $count);
+        } elseif($remaining === 0) {
+            $count++;
+        } else {
+            $count = 0;
+        }
+    }
+}
+
+countCombinations($data, 150, $count);
+
+echo 'Answer: ' . $count;
