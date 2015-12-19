e483fbe7db2c9ef10ba5e335f3d611c99771dc4f
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-12-18 21:57:26 -0800

    Add day 19 part 1

---
+++ day19/part1.php
@@ -0,0 +1,28 @@
+<?php
+
+$lines = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$map = array();
+$input = array_pop($lines);
+
+foreach($lines as $line) {
+    list($k, $v) = explode(' => ', $line);
+    if(!array_key_exists($k, $map)) {
+        $map[$k] = array();
+    }
+    $map[$k][] = $v;
+}
+
+$output = array();
+foreach($map as $el => $reps) {
+   $len = strlen($el);
+   foreach($reps as $rep) {
+        $offset = 0;
+        while(($offset = strpos($input, $el, $offset)) !== false) {
+            $new = substr($input, 0, $offset) . $rep . substr($input, $offset + $len);
+            $output[$new] = true;
+            $offset += $len;
+        }
+    }
+}
+
+echo 'Answer: ' . count($output);
