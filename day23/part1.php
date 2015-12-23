cbd57f713c0b8823bc15f0df21052259f5da24f5
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-12-22 21:34:02 -0800

    Add day 23

---
+++ day23/part1.php
@@ -0,0 +1,47 @@
+<?php
+
+$input = file('input.txt', FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+
+$r = array(
+    'a' => 0,
+    'b' => 0
+);
+$i = 0;
+while($i < count($input)) {
+    list($cmd, $param) = explode(' ', $input[$i], 2);
+    switch($cmd) {
+        case 'hlf':
+            $r[$param] = (int)($r[$param] / 2);
+            $i++;
+            break;
+        case 'tpl':
+            $r[$param] *= 3;
+            $i++;
+            break;
+        case 'inc':
+            $r[$param]++;
+            $i++;
+            break;
+        case 'jmp':
+            $i += intval($param);
+            break;
+        case 'jie':
+            list($p1, $p2) = explode(', ', $param, 2);
+            if($r[$p1] % 2 === 0) {
+                $i += intval($p2);
+            } else {
+                $i++;
+            }
+            break;
+        case 'jio':
+            list($p1, $p2) = explode(', ', $param, 2);
+            if($r[$p1] === 1) {
+                $i += intval($p2);
+            } else {
+                $i++;
+            }
+            break;
+    }
+}
+
+echo 'Answer: ' . $r['b'];
